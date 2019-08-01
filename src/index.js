bddd41638b2e165c2c866614e38e2bf0689a539b
xboudsady <boudsadyx@gmail.com>
2019-08-01 02:27:04 +0200

    craeted state object to hold our latitude, will initial value of null

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.sate = {
+      lat: null
+    }
+  }
+
+  // React says we have to define render!!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
