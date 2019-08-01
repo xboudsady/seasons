e1a8edbf5a062546ceed3856a7b3a3fe6230926f
xboudsady <boudsadyx@gmail.com>
2019-08-01 14:43:41 +0200

    Added conditional rendering to render()

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor(props) {
+  constructor(props) {                                        // Creat our state, with two default properties
     super(props);
 
     // THIS IS THE ONLY TIME we do direct assignment
     // to this.state
     this.state = { lat: null, errorMessage: '' };
 
-    window.navigator.geolocation.getCurrentPosition(
-      position => {
+    window.navigator.geolocation.getCurrentPosition(          // Browser API to get geolocation via the windows object
+      (position) => {
         // we called setState!!
         this.setState({ lat: position.coords.latitude });
       },
-      err => {
+      (err) => {                                              // Error catching as second argument
         this.setState({ errorMessage: err.message })
       }
     );
@@ -22,13 +22,15 @@ class App extends React.Component {
 
   // React says we have to define render!!
   render() {
-    return (
-      <div>
-        Latitude: { this.state.lat }
-        <br />
-        Error: { this.state.errorMessage}
-      </div>
-    );
+      if (this.state.errorMessage && !this.state.lat) {           // If no latitude is available
+        return <div>Error: { this.state.errorMessage } </div>
+      }
+      
+      if (!this.state.errorMessage && this.state.lat) {           // If latitude is avaialable
+        return <div>Latitude: { this.state.lat }</div>
+      } 
+      
+      return <div>Loading!</div>                                  // Loading 
   }
 }
 
