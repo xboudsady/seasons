b01cb5f8d4531199cbfaef4ad4a1d06118a5fadd
xboudsady <boudsadyx@gmail.com>
2019-08-04 14:06:49 +0200

    Created configuration object to store return text for season. Refactored dduplicate return function for text string for season, and icon name.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+// Configuration Object -- to return string value to use in return calls
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr it is cold!',
+    iconName: 'snowflak'
+  },
+};
+
+
 // Return an Integer value, to determine if its witer or summer
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -12,17 +25,17 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = props => {
   // Grab the string value, based on go location to determine season
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+  
+  // Deconstruct our key name from object
+  const { text, iconName } = seasonConfig[season]  // Return our object with the { text, iconName }
 
-  // Store value of icon class as string in a variable called icon
-  const icon = season === 'winter?' ? 'snowflake' : 'sun';
 
   // If condition, to determine the value of the season variable from the getSesason()
   return (
     <div>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
     </div>
   )
 };
