48a8d369c8ecc992e4dcf4c07319ae0903b04507
xboudsady <boudsadyx@gmail.com>
2019-08-04 13:40:34 +0200

    Added return function to display text based on winter or summer

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Return an Integer value, to determine if its witer or summer
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter';
@@ -9,11 +10,16 @@ const getSeason = (lat, month) => {
 };
 
 const SeasonDisplay = props => {
+  // Grab the string value, based on go location to determine season
   const season = getSeason(props.lat, new Date().getMonth());
+  const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
 
-  console.log(season);
-
-  return <div>Season Display</div>
+  // If condition, to determine the value of the season variable from the getSesason()
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
 };
 
 export default SeasonDisplay;
